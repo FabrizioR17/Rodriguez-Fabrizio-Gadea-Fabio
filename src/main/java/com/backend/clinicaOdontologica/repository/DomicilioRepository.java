024b01a663c161bfe11e2cc95f32a2bcddadb69f
FabrizioR17 <fabriziora12@gmail.com>
2024-06-13 18:07:32 -0500

    añadiendo una lista despegable en editarturno.js para poder tambien modifcar el odontologo cuando modificas el turno

--- src/main/java/com/backend/clinicaOdontologica/repository/DomicilioRepository.java
+++ src/main/java/com/backend/clinicaOdontologica/repository/DomicilioRepository.java
@@ -1,8 +1,8 @@
-package com.backend.clinicaOdontologica.repository;
-
-import com.backend.clinicaOdontologica.entity.Domicilio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface DomicilioRepository extends JpaRepository<Domicilio, Long> {
-}
+//package com.backend.clinicaOdontologica.repository;
+//
+//import com.backend.clinicaOdontologica.entity.Domicilio;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//
+//public interface DomicilioRepository extends JpaRepository<Domicilio, Long> {
+//}
