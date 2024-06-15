11bf2d5d022ccf90e89cc0576b3c3b36058f50f9
Fabrizio Rodriguez <fabriziora12@gmail.com>
2024-06-15 11:06:39 -0500

    test de odontologo y crando carpeta resources en test

--- src/test/java/com/backend/clinicaOdontologica/service/impl/OdontologoServiceTest.java
+++ src/test/java/com/backend/clinicaOdontologica/service/impl/OdontologoServiceTest.java
@@ -1,33 +1,88 @@
+//package com.backend.clinicaOdontologica.service.impl;
+//
+//import com.backend.clinicaOdontologica.dto.entrada.OdontologoEntradaDto;
+//import com.backend.clinicaOdontologica.dto.salida.OdontologoSalidaDto;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.Assert.*;
+//
+//@SpringBootTest
+//public class OdontologoServiceTest {
+//
+//    @Autowired
+//    private OdontologoService odontologoService;
+//
+//   @Test
+//    void deberiaRetornarseUnaListaNoVaciaDeOdontologosEnH2() {
+//        assertFalse(odontologoService.listarOdontologos().isEmpty());
+//    }
+//
+//    @Test
+//    void deberiaRegistrarseUnOdontologoYObtenerElIdCorrespondiente() {
+//
+//        OdontologoEntradaDto odontologo = new OdontologoEntradaDto( 12345, "Juan", "Perez");
+//
+//        OdontologoSalidaDto odontolgoRegistrado = odontologoService.registrarOdontologo(odontologo);
+//
+//        assertNotNull(odontolgoRegistrado.getId());
+//
+//    }
+//
+//}
 package com.backend.clinicaOdontologica.service.impl;
-
 import com.backend.clinicaOdontologica.dto.entrada.OdontologoEntradaDto;
 import com.backend.clinicaOdontologica.dto.salida.OdontologoSalidaDto;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.Assert.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class OdontologoServiceTest {
+@TestPropertySource(locations = "classpath:application-test.properties")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class OdontologoServiceTest {
 
     @Autowired
     private OdontologoService odontologoService;
 
-   @Test
-    void deberiaRetornarseUnaListaNoVaciaDeOdontologosEnH2() {
-        assertFalse(odontologoService.listarOdontologos().isEmpty());
+    @Test
+    @Order(1)
+    void deberiaRegistrarseUnOdontologoDeNombreJuan_yRetornarSuId() {
+        OdontologoEntradaDto odontologoEntradaDto = new OdontologoEntradaDto(1001, "Juan", "Perez");
+        OdontologoSalidaDto odontologoSalidaDto = odontologoService.registrarOdontologo(odontologoEntradaDto);
+
+        // assert
+        assertNotNull(odontologoSalidaDto);
+        assertNotNull(odontologoSalidaDto.getId());
+        assertEquals("Juan", odontologoSalidaDto.getNombre());
     }
 
     @Test
-    void deberiaRegistrarseUnOdontologoYObtenerElIdCorrespondiente() {
-
-        OdontologoEntradaDto odontologo = new OdontologoEntradaDto( 12345, "Juan", "Perez");
-
-        OdontologoSalidaDto odontolgoRegistrado = odontologoService.registrarOdontologo(odontologo);
-
-        assertNotNull(odontolgoRegistrado.getId());
-
+    @Order(2)
+    void deberiaDevolverUnaListaNoVaciaDeOdontologos() {
+        List<OdontologoSalidaDto> listadoDeOdontologos = odontologoService.listarOdontologos();
+        assertFalse(listadoDeOdontologos.isEmpty());
     }
 
+    @Test
+    @Order(3)
+    void deberiaEliminarseElOdontologoConId1() {
+        assertDoesNotThrow(() -> odontologoService.eliminarOdontologo(1L));
+    }
+
+    @Test
+    @Order(4)
+    void deberiaDevolverUnaListaVaciaDeOdontologos() {
+        List<OdontologoSalidaDto> listadoDeOdontologos = odontologoService.listarOdontologos();
+        assertTrue(listadoDeOdontologos.isEmpty());
+    }
 }
--- src/test/java/com/backend/clinicaOdontologica/service/impl/OdontologoServiceTestUsandoJunit4.java
+++ src/test/java/com/backend/clinicaOdontologica/service/impl/OdontologoServiceTestUsandoJunit4.java
@@ -1,37 +1,37 @@
-package com.backend.clinicaOdontologica.service.impl;
-
-import com.backend.clinicaOdontologica.dto.entrada.OdontologoEntradaDto;
-import com.backend.clinicaOdontologica.dto.salida.OdontologoSalidaDto;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
-public class OdontologoServiceTestUsandoJunit4 {
-    
-    @Autowired
-    OdontologoService odontologoService;
-    
-    @Test
-    public void listarOdontologos2() {
-        assertFalse(odontologoService.listarOdontologos().isEmpty());
-
-    }
-    
-    @Test
-    public void deberiablabla () {
-
-        OdontologoEntradaDto odontologo = new OdontologoEntradaDto(12345, "Juan", "Perez");
-        OdontologoSalidaDto odontologoRegistrado = odontologoService.registrarOdontologo(odontologo);
-
-        assertNotNull(odontologoRegistrado.getId());
-
-    }
-
-}
+//package com.backend.clinicaOdontologica.service.impl;
+//
+//import com.backend.clinicaOdontologica.dto.entrada.OdontologoEntradaDto;
+//import com.backend.clinicaOdontologica.dto.salida.OdontologoSalidaDto;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//@SpringBootTest
+//@RunWith(SpringJUnit4ClassRunner.class)
+//public class OdontologoServiceTestUsandoJunit4 {
+//
+//    @Autowired
+//    OdontologoService odontologoService;
+//
+//    @Test
+//    public void listarOdontologos2() {
+//        assertFalse(odontologoService.listarOdontologos().isEmpty());
+//
+//    }
+//
+//    @Test
+//    public void deberiablabla () {
+//
+//        OdontologoEntradaDto odontologo = new OdontologoEntradaDto(12345, "Juan", "Perez");
+//        OdontologoSalidaDto odontologoRegistrado = odontologoService.registrarOdontologo(odontologo);
+//
+//        assertNotNull(odontologoRegistrado.getId());
+//
+//    }
+//
+//}
